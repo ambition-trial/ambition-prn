00d6ef37ffdaabe42d7b7bba304c08e052f0399c
erikvw <ew2789@gmail.com>
2018-11-11 21:46:10 -0600

    fix study termination form action recreating itself, add display names

--- ambition_prn/action_items.py
+++ ambition_prn/action_items.py
@@ -1,10 +1,9 @@
 from ambition_ae.constants import AE_INITIAL_ACTION, AE_FOLLOWUP_ACTION
 from ambition_subject.constants import BLOOD_RESULTS_ACTION
 from django.apps import apps as django_apps
-from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from django.utils.safestring import mark_safe
-from edc_action_item import Action, HIGH_PRIORITY, site_action_items
+from edc_action_item import ActionWithNotification, HIGH_PRIORITY, site_action_items
 from edc_constants.constants import CLOSED
 
 from .constants import DEATH_REPORT_ACTION, DEATH_REPORT_TMG_ACTION
@@ -13,9 +12,10 @@ from .constants import STUDY_TERMINATION_CONCLUSION_ACTION
 from .constants import STUDY_TERMINATION_CONCLUSION_ACTION_W10
 
 
-class ProtocolDeviationViolationAction(Action):
+class ProtocolDeviationViolationAction(ActionWithNotification):
     name = PROTOCOL_DEVIATION_VIOLATION_ACTION
     display_name = 'Submit Protocol Deviation/Violation Report'
+    notification_display_name = 'Protocol Deviation/Violation Report'
     parent_action_names = []
     reference_model = 'ambition_prn.protocoldeviationviolation'
     show_link_to_changelist = True
@@ -27,9 +27,10 @@ class ProtocolDeviationViolationAction(Action):
         return self.reference_obj.report_status == CLOSED
 
 
-class StudyTerminationConclusionAction(Action):
+class StudyTerminationConclusionAction(ActionWithNotification):
     name = STUDY_TERMINATION_CONCLUSION_ACTION
     display_name = 'Submit Study Termination/Conclusion Report'
+    notification_display_name = 'Study Termination/Conclusion Report'
     parent_action_names = [BLOOD_RESULTS_ACTION, DEATH_REPORT_ACTION]
     reference_model = 'ambition_prn.studyterminationconclusion'
     show_link_to_changelist = True
@@ -37,9 +38,10 @@ class StudyTerminationConclusionAction(Action):
     priority = HIGH_PRIORITY
 
 
-class StudyTerminationConclusionW10Action(Action):
+class StudyTerminationConclusionW10Action(ActionWithNotification):
     name = STUDY_TERMINATION_CONCLUSION_ACTION_W10
     display_name = 'Submit W10 Study Termination/Conclusion Report'
+    notification_display_name = 'W10 Study Termination/Conclusion Report'
     parent_action_names = [DEATH_REPORT_ACTION]
     reference_model = 'ambition_prn.studyterminationconclusionw10'
     show_link_to_changelist = True
@@ -47,9 +49,10 @@ class StudyTerminationConclusionW10Action(Action):
     priority = HIGH_PRIORITY
 
 
-class DeathReportAction(Action):
+class DeathReportAction(ActionWithNotification):
     name = DEATH_REPORT_ACTION
     display_name = 'Submit Death Report'
+    notification_display_name = 'Death Report'
     reference_model = 'ambition_prn.deathreport'
     parent_action_names = [AE_INITIAL_ACTION, AE_FOLLOWUP_ACTION]
     show_link_to_changelist = True
@@ -60,8 +63,10 @@ class DeathReportAction(Action):
     dirty_fields = ['cause_of_death']
 
     def get_next_actions(self):
-        """Adds 1 DEATHReportTMG if not yet created.
+        """Adds 1 DEATHReportTMG if not yet created and
+        STUDY_TERMINATION_CONCLUSION if required.
         """
+        # DEATH_REPORT_TMG_ACTION
         try:
             self.action_item_model_cls().objects.get(
                 parent_action_item=self.reference_obj.action_item,
@@ -72,21 +77,35 @@ class DeathReportAction(Action):
         else:
             next_actions = []
 
+        # STUDY_TERMINATION_CONCLUSION_ACTION
         on_schedule_w10_model_cls = django_apps.get_model(
             'ambition_prn.onschedulew10')
+        off_schedule_w10_cls = django_apps.get_model(
+            'ambition_prn.studyterminationconclusionw10')
+        off_schedule_cls = django_apps.get_model(
+            'ambition_prn.studyterminationconclusion')
         try:
             on_schedule_w10_model_cls.objects.get(
                 subject_identifier=self.subject_identifier)
         except ObjectDoesNotExist:
-            next_actions.append(STUDY_TERMINATION_CONCLUSION_ACTION)
+            try:
+                off_schedule_cls.objects.get(
+                    subject_identifier=self.subject_identifier)
+            except ObjectDoesNotExist:
+                next_actions.append(STUDY_TERMINATION_CONCLUSION_ACTION)
         else:
-            next_actions.append(STUDY_TERMINATION_CONCLUSION_ACTION_W10)
+            try:
+                off_schedule_w10_cls.objects.get(
+                    subject_identifier=self.subject_identifier)
+            except ObjectDoesNotExist:
+                next_actions.append(STUDY_TERMINATION_CONCLUSION_ACTION_W10)
         return next_actions
 
 
-class DeathReportTmgAction(Action):
+class DeathReportTmgAction(ActionWithNotification):
     name = DEATH_REPORT_TMG_ACTION
     display_name = 'TMG Death Report pending'
+    notification_display_name = 'TMG Death Report'
     parent_action_names = [DEATH_REPORT_ACTION, DEATH_REPORT_TMG_ACTION]
     reference_model = 'ambition_prn.deathreporttmg'
     related_reference_model = 'ambition_prn.deathreport'
@@ -98,10 +117,10 @@ class DeathReportTmgAction(Action):
     admin_site_name = 'ambition_prn_admin'
     instructions = mark_safe(
         f'This report is to be completed by the TMG only.')
-    try:
-        email_recipients = [settings.EMAIL_CONTACTS.get('tmg')]
-    except AttributeError:
-        email_recipients = []
+#     try:
+#         email_recipients = [settings.EMAIL_CONTACTS.get('tmg')]
+#     except AttributeError:
+#         email_recipients = []
 
     def close_action_item_on_save(self):
         if (self.reference_obj.death_report.cause_of_death
