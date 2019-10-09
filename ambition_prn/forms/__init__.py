282e1584147cb88f862c283b4fe0a8c81253040e
erikvw <ew2789@gmail.com>
2019-10-09 00:36:09 -0500

    add to admin

--- ambition_prn/forms/__init__.py
+++ ambition_prn/forms/__init__.py
@@ -1,6 +1,7 @@
 from .amphotericin_missed_doses_form import AmphotericinMissedDosesForm
 from .death_report_form import DeathReportForm
 from .death_report_tmg_form import DeathReportTmgForm
+from .death_report_tmg_second_form import DeathReportTmgSecondForm
 from .fluconazole_missed_doses_form import FluconazoleMissedDosesForm
 from .flucytosine_missed_doses_form import FlucytosineMissedDosesForm
 from .protocol_deviation_violation_form import ProtocolDeviationViolationForm
